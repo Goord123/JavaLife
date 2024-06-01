3ecd05b15f48a85fa9e67c0be31cb755a6541e75
Diogo <diogofgf97@gmail.com>
2024-06-01 08:09:19 +0100

    merge

--- src/main/java/org/isec/pa/ecossistema/model/data/Flora.java
+++ src/main/java/org/isec/pa/ecossistema/model/data/Flora.java
@@ -1,34 +1,37 @@
 package org.isec.pa.ecossistema.model.data;
 
-import org.isec.pa.ecossistema.model.EcossistemaManager;
 import org.isec.pa.ecossistema.utils.Area;
 import org.isec.pa.ecossistema.utils.ElementoEnum;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.List;
 
 public non-sealed class Flora extends ElementoBase implements IElemento, Serializable, IElementoComImagem, IElementoComForca {
-
+    @Serial
+    private static final long serialVersionUID = 1L;
     private static int lastId = 0; // Static variable to keep track of the last ID used
     private final ElementoEnum elementoEnum = ElementoEnum.FLORA;
     private int id;
-    private double forca = 50;
+    private double forca;
     private int size;
     private int timesReproduced = 0;
     private Area area;
     private String imagem;
     private Ecossistema ecossistema;
     private boolean dead = false;
+    private double forcaPorTurno;
 
     public Flora(Ecossistema ecossistema) {
         this.ecossistema = ecossistema;
+        this.forca = ecossistema.getForcaDefault();
         this.id = ++lastId;
+        this.forcaPorTurno = 1;
     }
 
     public void evolve() {
         if (this.forca <= 0) dead = true;
 
-        this.forca++;
+        this.forca = this.forca + forcaPorTurno;
         if (this.forca > 100) this.forca = 100;
 
         reproduce();
@@ -88,6 +91,14 @@ public non-sealed class Flora extends ElementoBase implements IElemento, Seriali
 
     public void setDead(boolean dead) {
         this.dead = dead;
+    }
+
+    public double getForcaPorTurno() {
+        return forcaPorTurno;
+    }
+
+    public void setForcaPorTurno(double forcaPorTurno) {
+        this.forcaPorTurno = forcaPorTurno;
     }
 
     @Override
@@ -152,5 +163,15 @@ public non-sealed class Flora extends ElementoBase implements IElemento, Seriali
             }
         }
         return false;
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
     }
 }
--- src/main/java/org/isec/pa/ecossistema/model/fsm/FaunaContext.java
+++ src/main/java/org/isec/pa/ecossistema/model/fsm/FaunaContext.java
@@ -3,23 +3,29 @@ package org.isec.pa.ecossistema.model.fsm;
 import org.isec.pa.ecossistema.model.data.Fauna;
 import org.isec.pa.ecossistema.model.fsm.FaunaStates.IFaunaState;
 
-public class FaunaContext {
+import java.io.Serial;
+import java.io.Serializable;
 
+public class FaunaContext implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private Fauna fauna;
     private IFaunaState currentState;
-    public FaunaContext(Fauna fauna){
+
+    public FaunaContext(Fauna fauna) {
         this.fauna = fauna;
         this.currentState = FaunaState.LOOKING_FOR_FLORA.getInstance(this, fauna);
         this.fauna.changeState(currentState);
     }
 
-    public void changeState(IFaunaState newState){
+    public void changeState(IFaunaState newState) {
         this.currentState = newState;
     }
 
     public void evolve() {
         currentState.evolve();
     }
+
     public IFaunaState getCurrentState() {
         return fauna.getCurrentState();
     }
